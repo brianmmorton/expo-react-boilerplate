99bd39f631cf79961890d355d25ac27bac38a63b
Brian Morton <brianmmorton@gmail.com>
2017-08-27 13:38:23 -0700

    reset reducers

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -2,7 +2,3 @@
 
 export user from './user'
 export token from './token'
-export clusters from './clusters'
-export projects from './projects'
-export monthlyStats from './monthly-stats'
-export timesheet from './timesheet'
